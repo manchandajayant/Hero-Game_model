6084e34f95ea04d9f3701503fbf240f70b09f38a
manchandajayant <maanjayant@gmail.com>
2020-02-01 12:23:27 +0100

    2 parts done

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,2 +1,25 @@
 // Write your JS here
 const myHero = {};
+const hero = {
+  name: "",
+  heroic: true,
+  inventory: [],
+  health: 10,
+  weapon: {
+    type: "",
+    damage: 2
+  }
+};
+
+function rest(person) {
+  const newHealth = 10;
+  person === newHealth;
+  return person;
+  if (newHealth === 10) {
+    alert("health is already 10");
+  }
+}
+
+function pickUpItem() {}
+
+function equipWeapon() {}
