8f06c655e31cf10dc0189d257521b98c930a6fa6
manchandajayant <maanjayant@gmail.com>
2020-02-01 22:02:45 +0100

    not-completed game-file

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -11,15 +11,59 @@ const hero = {
   }
 };
 
-function rest(person) {
-  const newHealth = 10;
-  person === newHealth;
-  return person;
-  if (newHealth === 10) {
-    alert("health is already 10");
-  }
+const person = hero;
+const wepon = hero.weapon;
+console.log(person.weapon);
+
+function submitName() {
+  const button = document.getElementById("button");
+  const p = document.createElement("p");
 }
 
-function pickUpItem() {}
+function healthToTen() {
+  hero.health = 10;
+}
 
-function equipWeapon() {}
+function weapon() {
+  if (dagger()) person.inventory.push(person.weapon);
+  person.weapon.type = "dagger";
+}
+
+function sword() {
+  person.inventory.push(person.weapon);
+  person.weapon.type = "sword";
+}
+
+function bowArrow() {
+  person.inventory.push(person.weapon);
+  person.weapon.type = "BowArrow";
+}
+
+function rest(person) {
+  if (person.health === 10) {
+    alert("health is already 10");
+  } else {
+    person.health = 10;
+  }
+  return person;
+}
+
+function pickUpItem(person, wepon) {
+  person.inventory.push(wepon);
+}
+
+function equipWeapon(person) {
+  person.inventory.unshift(wepon);
+}
+
+function firstItemOnInventory() {
+  person.weapon = hero.inventory[0];
+}
+
+function displayStats() {
+  const inventory = document.getElementById("yourweapon");
+  const health = document.getElementById("health");
+
+  const healthLevel = document.createElement("p");
+  const yourWeapons = document.createElement("p");
+}
